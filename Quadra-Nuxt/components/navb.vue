011371c62106c8ce32baf01ff616cbd3dd08c5eb
Rafael <rafaelazumiyoung@gmail.com>
2024-09-05 15:22:10 -0300

    Começando pela navbar

---
+++ Quadra-Nuxt/components/navb.vue
@@ -0,0 +1,16 @@
+<template>
+
+<header class="cabecalho">
+        <nav class="cabecalho_menu">
+            <img class="cabecalho_menu_logo" src="../public/logo-navbar.png" alt="" width="160px">
+            <a class="cabecalho_menu_link" href="index.html">Home <i class="fa-solid fa-house"></i></a>
+            <a class="cabecalho_menu_link" href="assets/html/Quadras.html">Quadras <i class="fa-solid fa-globe"></i></a>
+            <a class="cabecalho_menu_link" href="assets/html/Historia.html">Quem Somos <i class="fa-solid fa-question"></i></a>
+            <a class="cabecalho_menu_link" href="assets/html/Quadras.html">Download <i class="fa-solid fa-circle-down"></i></a> 
+            <a class="cabecalho_menu_link" href="assets/html/Quadras.html">Contato <i class="fa-solid fa-phone"></i></a>
+            <a class="cabecalho_menu_link_login" href="assets/html/Login.html">Login/Área Exclusiva <i class="fa-solid fa-user"></i></a>
+        </nav>
+    </header>
+
+
+</template>
