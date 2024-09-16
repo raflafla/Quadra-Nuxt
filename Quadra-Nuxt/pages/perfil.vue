6c5c74a9f0f1065c0abd7ad30e8aeebfe863d4cd
Paulo-NB <nettobrito55@gmail.com>
2024-09-16 14:54:18 -0300

    att perfil

--- Quadra-Nuxt/pages/perfil.vue
+++ Quadra-Nuxt/pages/perfil.vue
@@ -3,8 +3,67 @@
 @import url('../assets/perfil.css');
 </style>
 
+
+
+
+
+
+
+
 <template>
 
-    <h1>teste</h1>
+
+<section class="cards-container">
+        <a href="###" class="card">
+            <div class="card-header">
+                <img src="https://pbs.twimg.com/media/EXNwfE_WoAMarG5.jpg" alt="Foto de Perfil do Cliente" class="perfil-imagem"/>
+            </div>
+            <div class="card-content">
+                
+                <p class="card-description">Bahia</p>
+            </div>
+        </a>
+    </section>
+
+    <section class="cards-container">
+        <a href="###" class="card">
+            <div class="card-header">
+                <i class="fa-solid fa-address-card"></i>
+            </div>
+            <div class="card-content">
+                <h2 class="card-title">Meus Dados</h2>
+                <p class="card-description">Confira as suas informações aqui.</p>
+            </div>
+        </a>
+    </section>
+
+    <section class="cards-container">
+
+        <a href="###" class="card">
+            <div class="card-header">
+                <i class="fa-solid fa-clipboard-list"></i>
+            </div>
+            <div class="card-content">
+                <h2 class="card-title">Histórico de Quadras</h2>
+                <p class="card-description">Confira o seu histórico aqui.</p>
+            </div>
+        </a>
+    </section>
+
+
+    <section class="cards-container">
+
+    <a href="###" class="card">
+            <div class="card-header">
+                <i class="fa-solid fa-pager"></i>
+            </div>
+            <div class="card-content">
+                <h2 class="card-title">Pagamento</h2>
+                <p class="card-description">Confira o seu pagamento aqui.</p>
+            </div>
+        </a>
+    </section>
+
+   
 
 </template>
