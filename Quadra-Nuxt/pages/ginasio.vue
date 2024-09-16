0138a2ba0095b5b23cfc3c912f8b35e6d8a0d2db
juanvictor-jv <juansilva282129@gmail.com>
2024-09-16 17:15:12 -0300

    ginasio

---
+++ Quadra-Nuxt/pages/ginasio.vue
@@ -0,0 +1,50 @@
+<style>
+
+    @import url(../assets/ginasio.css);
+
+
+</style>
+
+
+<template>
+
+    <section>
+        <h2 class="div1"> <strong>Ginásio Redenção</strong></h2>
+        <div class="conteiner">
+
+            <img src="redencao.jfif" class="img"/>
+
+            <div class="div2">
+                <h3> <strong> Quadra </strong> 4 </h3>
+                <p>Uma quadra para você jogar com amigos, com seu time em Campeonato!</p>
+                <p> <strong> Preço </strong> </p>
+                <p> <strong>R$ </strong> 150,50</p>
+
+                <input type="date" class="cabecalho_menu_link_login"> - <input type="time" class="cabecalho_menu_link_login">
+
+                <br/>
+                <br/>
+
+                <a href="#" class="cabecalho_menu_link_login">Alugar</a>
+            </div>
+
+        </div>
+
+        <br/>
+        <br/>
+
+        <p class="p3">
+            O ginásio foi viabilizados através de um Decreto Municipal.
+            <br/>
+            Em 28 de novembro de 2010, o então prefeito Oswaldo Barba, 
+            <br/>
+            inaugurou os novos vestiários do ginásio que foram totalmente 
+            <br/>
+            remodelados, e a reforma da cobertura.
+        </p>
+
+    </section>
+
+
+
+</template>
