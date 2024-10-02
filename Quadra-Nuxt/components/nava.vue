df6d87ba6368fd2b0e9804166bbd2e7a2c9f28b5
Rafael Carlos <rafaelazumiyoung@gmail.com>
2024-10-02 14:08:59 -0300

    navbar corrigida

--- Quadra-Nuxt/components/nava.vue
+++ Quadra-Nuxt/components/nava.vue
@@ -13,8 +13,6 @@
             <NuxtLink class="cabecalho_menu_link" to="/">Home <i class="fa-solid fa-house"></i></NuxtLink>
             <NuxtLink class="cabecalho_menu_link" to="/quadras">Quadras <i class="fa-solid fa-globe"></i></NuxtLink>
             <NuxtLink class="cabecalho_menu_link" to="/historia">Quem Somos <i class="fa-solid fa-question"></i></NuxtLink>
-            <NuxtLink class="cabecalho_menu_link" to="/quadras">Download <i class="fa-solid fa-circle-down"></i></NuxtLink> 
-            <NuxtLink class="cabecalho_menu_link" to="/contato">Contato <i class="fa-solid fa-phone"></i></NuxtLink>
             <NuxtLink class="cabecalho_menu_link" to="/perfil">Perfil <i class="fa-solid fa-circle-user"></i></NuxtLink> 
         </nav>
     </header>
