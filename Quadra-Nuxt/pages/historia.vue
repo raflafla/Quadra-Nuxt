02ec000b752c50303a974c7694e30e643650525a
Rafael Carlos <rafaelazumiyoung@gmail.com>
2024-11-11 14:37:23 -0300

    historia

--- Quadra-Nuxt/pages/historia.vue
+++ Quadra-Nuxt/pages/historia.vue
@@ -7,13 +7,9 @@
 <template>
     <section class="historia-container">
         <div class="historia-container-a">
-            <h1 class="historia-titulo">Nossa História</h1>
-            <p class="historia-texto">Em uma cidade pequena, quatro amigos — Thayanan, Juan, Rafael, Paulo e Guilherme — estavam 
-            frustrados com a dificuldade de encontrar quadras esportivas disponíveis. Juan, sempre com um olhar inovador, s
-            ugeriu a criação de um site para reservas online, chamado "Quadra Finder". A ideia ganhou força rapidamente entre os amigos. Rafael, 
-            com seu talento para negociação, estabeleceu parcerias com proprietários de quadras, enquanto os cinco amigos se dedicavam ao desenvolvimento do site. 
-            Guilherme, com sua expertise em design e marketing, cuidou da aparência e da divulgação da plataforma.</p>
-            <img src="../public/quadra.jpg" alt="" class="historia-imagem">
+            <h1 class="historia-titulo">Quem somos?</h1>
+            <p class="historia-texto">Somos uma plataforma dedicada a conectar esportistas com quadras disponíveis para prática esportiva. No Quadra Finder, acreditamos que o acesso a espaços de qualidade deve ser fácil e rápido. Nosso objetivo é promover a saúde e o esporte, proporcionando uma experiência prática e acessível para que todos possam reservar quadras com segurança e comodidade.</p>
+            <img src="../public/shshshshs.webp" alt="" class="historia-imagem">
         </div>
     </section>
 </template>
