5c8e2ae688b472005b0a084da65c4dfbf7c9119e
Rafael Carlos <rafaelazumiyoung@gmail.com>
2024-11-11 14:40:09 -0300

    kkk

--- Quadra-Nuxt/components/navb.vue
+++ Quadra-Nuxt/components/navb.vue
@@ -9,7 +9,7 @@
             <img class="cabecalho_menu_logo" src="../public/logo-navbar.png" alt="" width="160px">
             <NuxtLink class="cabecalho_menu_link" to="/">Home <i class="fa-solid fa-house"></i></NuxtLink>
             <NuxtLink class="cabecalho_menu_link" to="/quadras">Quadras <i class="fa-solid fa-globe"></i></NuxtLink>
-            <NuxtLink class="cabecalho_menu_link" to="/historia">Quem Somos <i class="fa-solid fa-question"></i></NuxtLink>
+            <NuxtLink class="cabecalho_menu_link" to="">Quem Somos <i class="fa-solid fa-question"></i></NuxtLink>
             <NuxtLink class="cabecalho_menu_link_login" to="/login">Login/Área Exclusiva <i class="fa-solid fa-user"></i></NuxtLink>
         </nav>
     </header>
